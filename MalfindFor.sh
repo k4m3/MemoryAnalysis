04be7e71327513970acd31ad6c90a162650e3882
kelvem <30578900+k4m3@users.noreply.github.com>
2022-09-16 10:27:10 -0300

    Create MalfindFor.sh

---
+++ MalfindFor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+file_path=$1
+profile=$2
+
+for pid in $(python2.7 /opt/volatility/vol.py -f $1 --profile=$2 pslist | sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" | cut -d " " -f3 | egrep -v "PID|-" | sort | uniq)
+do python2.7 /opt/volatility/vol.py -f $1 --profile=$2 malfind -p $pid 2> /dev/null | egrep "^0x|Process|Vad|Flags" && echo ""
+done
