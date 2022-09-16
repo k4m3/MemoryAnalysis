17bec615b795ca54317ff61ebde6ae29d120c74c
kelvem <30578900+k4m3@users.noreply.github.com>
2022-09-16 10:46:07 -0300

    Update MalfindFor.sh

--- MalfindFor.sh
+++ MalfindFor.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-##Modo de usar $bash MalfindFor.sh file_path profile
-file_path=$1
-profile=$2
+##Use --> $bash MalfindFor.sh file.vmem
 
-for pid in $(python2.7 /opt/volatility/vol.py -f $1 --profile=$2 pslist | sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" | cut -d " " -f3 | egrep -v "PID|-" | sort | uniq)
-do python2.7 /opt/volatility/vol.py -f $1 --profile=$2 malfind -p $pid 2> /dev/null | egrep "^0x|Process|Vad|Flags" && echo ""
+file_path=$1
+
+profile=$(python2.7 /opt/volatility/vol.py -f $file_path imageinfo | grep "Suggested Profile" | cut -d "(" -f3 | cut -d " " -f3 | cut -d ")" -f1 2> /dev/null)
+
+
+for pid in $(python2.7 /opt/volatility/vol.py -f $file_path --profile=$profile pslist 2>/dev/null | sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" |  sed "s/  / /g" | cut -d " " -f3 | egrep -v "PID|-" | sort | uniq)
+do python2.7 /opt/volatility/vol.py -f $file_path --profile=$profile malfind -p $pid 2>/dev/null | egrep "^0x|Process|Vad|Flags" && echo ""
 done
